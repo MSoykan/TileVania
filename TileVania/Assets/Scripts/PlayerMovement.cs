99eaf96037d650b012410fd7d4793517a9bb0a34
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-05-15 16:20:52 +0300

    flip player sprite while running

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -7,21 +7,44 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
-
+    [SerializeField] float runSpeed = 6f;
+    [SerializeField] Rigidbody2D playerRigidBody;
+    Animator myAnimator;
 
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRigidBody.velocity.y);
+        playerRigidBody.velocity = playerVelocity;
+        bool playerHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("IsRunning", playerHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHorizontalSpeed = Mathf.Abs(playerRigidBody.velocity.x) > Mathf.Epsilon;
+
+
+        if (playerHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidBody.velocity.x), 1f);
+        }
     }
 }
