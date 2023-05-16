47f89e21db823c936647657505eb29d3b839650b
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-05-16 11:24:18 +0300

    Added Bullet and 3Levels
    Player can now shoot bullets and kill enemies(not using object pooling).Also added 3 different levels

---
+++ TileVania/Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] float bulletSpeed = 20f;
+    Rigidbody2D bulletRigidBody;
+    PlayerMovement player;
+    float xSpeed;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+        bulletRigidBody = GetComponent<Rigidbody2D>();
+        xSpeed = player.transform.localScale.x * bulletSpeed;
+    }
+
+    void Update()
+    {
+        bulletRigidBody.velocity = new Vector2 (xSpeed,0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runSpeed = 6f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
-    [SerializeField] Vector2 deathKick = new Vector2( 10f ,10f);
+    [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
 
 
     [SerializeField] Rigidbody2D playerRigidBody;
@@ -54,6 +56,12 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(moveInput);
     }
 
+    void OnFire(InputValue value)
+    {
+        if (!isAlive) { return; }   
+        Instantiate(bullet, gun.position, transform.rotation);
+    }
+
     void Run()
     {
         if (!isAlive) { return; }
@@ -95,12 +103,14 @@ public class PlayerMovement : MonoBehaviour
 
     void Die()
     {
-        if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
+        if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Enemies","Hazards")))
         {
             isAlive = false;
             myAnimator.SetTrigger("Dying");
             playerRigidBody.velocity = deathKick;
         }
     }
+
+
 
 }
